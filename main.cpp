3bd777d1335871ae0f417904ef202ee3eeb728c4
Göksu Guvendiren Bakir <goksuguvendiren@gmail.com>
2022-10-30 15:15:48 -0700

    update file names and gitignore

--- main.cpp
+++ main.cpp
@@ -67,11 +67,11 @@ int main(int argc, const char** argv)
     cv::imshow(scene.output_file_name(), image);
     cv::waitKey(0);
 
-    cv::imwrite(scene.output_hdr_name()  + "new_output.exr", image);
-    cv::imwrite(scene.output_file_name() + "new_output.png", image * 255);
+    cv::imwrite(scene.output_hdr_name()  + "_new_output.exr", image);
+    cv::imwrite(scene.output_file_name() + "_new_output.png", image * 255);
 
     auto tonemapped = soleil::tonemappers::gamma().process(image);
-    cv::imwrite(scene.output_file_name() + "tonemapped.png", tonemapped * 255);
+    cv::imwrite(scene.output_file_name() + "_tonemapped.png", tonemapped * 255);
 
     return 0;
 }
