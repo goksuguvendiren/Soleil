841cf95d85f9816a8e64fdeeccf1841234a4287e
Göksu Guvendiren Bakir <goksuguvendiren@gmail.com>
2022-10-30 20:48:54 -0700

    contd cleanup

--- include/integrators/metal.hpp
+++ include/integrators/metal.hpp
@@ -21,31 +21,11 @@ class metal
 {
 public:
     metal(unsigned int w, unsigned int h, int sq = 1);
-    
     std::vector<glm::vec3> render(const soleil::scene& scene);
-    glm::vec3 render_pixel(const soleil::scene& scene, int i, int j);
 
 private:
     int width = 100;
     int height = 100;
 };
-
-class mtk_renderer
-{
-public:
-    mtk_renderer(MTL::Device* device);
-    ~mtk_renderer();
-    void build_shaders();
-    void build_buffers();
-    void draw(MTK::View* pView);
-
-private:
-    MTL::Device* device;
-    MTL::CommandQueue* command_queue;
-    MTL::RenderPipelineState* pipeline_state;
-    MTL::Buffer* vertex_pos_buffer;
-    MTL::Buffer* vertex_color_buffer;
-};
-
 
 } // namespace soleil
